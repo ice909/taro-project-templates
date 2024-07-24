0f0dc9c443aff858c388ad447b6b01874e86ca06
huxiyang3 <huxiyang3@jd.com>
2024-07-24 17:23:21 +0800

    fix: style

--- react-NutUI/babel.config.js
+++ react-NutUI/babel.config.js
@@ -2,11 +2,12 @@
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
   presets: [
-    ['taro', {
-      framework: '{{ to_lower_case framework }}',
-      ts: {{ typescript }},
-      compiler: '{{ to_lower_case compiler }}',
-    }]
+    ['taro',
+      {
+        framework: '{{ to_lower_case framework }}',
+        ts: '{{ typescript }}',
+        compiler: '{{ to_lower_case compiler }}',
+      }]
   ],
   plugins: [
     [
