ed3ace30fbbbde1bf054f7569d1c520444f6f2bf
huxiyang3 <huxiyang3@jd.com>
2024-08-06 11:23:19 +0800

    fix: update

--- react-NutUI/src/pages/index/index.jsx
+++ react-NutUI/src/pages/index/index.jsx
@@ -1,39 +1,61 @@
-import React, { useState } from 'react'
-import { View } from '@tarojs/components'
-import { Button } from '@nutui/nutui-react-taro'
-import { useTranslate } from '@/sites/assets/locale/taro'
-import enUS from '@nutui/nutui-react-taro/dist/locales/en-US.ts'
-import zhCN from '@nutui/nutui-react-taro/dist/locales/zhCN'
-import './index.{{ cssExt }}'
+import React, { useState } from "react";
+import { View } from "@tarojs/components";
+import {
+  Button,
+  ConfigProvider,
+  TextArea,
+  Dialog,
+  Cell
+} from "@nutui/nutui-react-taro";
+import enUS from "@nutui/nutui-react-taro/dist/locales/en-US";
+import zhCN from "@nutui/nutui-react-taro/dist/locales/zh-CN";
+import "./index.scss";
 function Index() {
-  const [locale, setLocale] = useState(zhCN)
-  const [translated] = useTranslate({
-    'zh-CN': {
-      welcome: '欢迎使用 NutUI React 开发 Taro 多端项目。',
-      button: 'NutUI React 按钮',
+  const [locale, setLocale] = useState(zhCN);
+  const localeKey = locale === zhCN ? 'zhCN' : 'enUS'
+  const [visible, setVisible] = useState(false);
+  const [translated] = useState({
+    zhCN: {
+      welcome: "欢迎使用 NutUI React 开发 Taro 多端项目。",
+      button: "使用英文",
+      open: '点击打开'
     },
-    'en-US': {
-      welcome: 'Welcome to use NutUI React to develop Taro multi-terminal projects.',
-      button: 'NutUI React Button',
+    enUS: {
+      welcome:
+        "Welcome to use NutUI React to develop Taro multi-terminal projects.",
+      button: "Use Chinese",
+      open: 'Click Me'
     },
-  })
+  });
   // const context = useConfig()
   const handleSwitchLocale = () => {
-    setLocale(locale === zhCN ? enUS : zhCN)
-  }
+    setLocale(locale === zhCN ? enUS : zhCN);
+  };
   return (
-    <Configprovider locale={locale}>
-      <View className='nutui-react-demo'>
-        <View>{translated.welcome}</View>
+    <ConfigProvider locale={locale}>
+      <View className="nutui-react-demo">
+        <Cell>
+          <h3>{translated[localeKey].welcome}</h3>
+        </Cell>
         <View>
-          <Button type='primary' onClick={handleSwitchLocale}>
-            {translated.button}
+          <Button type="primary" onClick={handleSwitchLocale} style={{marginRight: 10}}>
+            {translated[localeKey].button}
           </Button>
+          <Button type="success" onClick={() => setVisible(true)}>
+            {translated[localeKey].open}
+          </Button>
+          <Dialog
+            visible={visible}
+            onConfirm={() => setVisible(false)}
+            onCancel={() => setVisible(false)}
+          >
+            {translated[localeKey].welcome}
+          </Dialog>
           <TextArea disabled showCount maxLength={20} />
         </View>
       </View>
-    </Configprovider>
-  )
+    </ConfigProvider>
+  );
 }
 
-export default Index
+export default Index;
