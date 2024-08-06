c8332447bbf9c1e37f5d13e53f071d11966a86ea
huxiyang3 <huxiyang3@jd.com>
2024-08-06 10:58:35 +0800

    fix: update multilang

--- react-NutUI/src/pages/index/index.jsx
+++ react-NutUI/src/pages/index/index.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
+import { Button } from '@nutui/nutui-react-taro'
+import { useTranslate } from '@/sites/assets/locale/taro'
+import enUS from '@nutui/nutui-react-taro/dist/locales/en-US.ts'
+import zhCN from '@nutui/nutui-react-taro/dist/locales/zhCN'
 import './index.{{ cssExt }}'
-
 function Index() {
+  const [locale, setLocale] = useState(zhCN)
+  const [translated] = useTranslate({
+    'zh-CN': {
+      welcome: '欢迎使用 NutUI React 开发 Taro 多端项目。',
+      button: 'NutUI React 按钮',
+    },
+    'en-US': {
+      welcome: 'Welcome to use NutUI React to develop Taro multi-terminal projects.',
+      button: 'NutUI React Button',
+    },
+  })
+  // const context = useConfig()
+  const handleSwitchLocale = () => {
+    setLocale(locale === zhCN ? enUS : zhCN)
+  }
   return (
-    <View className="nutui-react-demo">
-      <View>
-        欢迎使用 NutUI React 开发 Taro 多端项目。
+    <Configprovider locale={locale}>
+      <View className='nutui-react-demo'>
+        <View>{translated.welcome}</View>
+        <View>
+          <Button type='primary' onClick={handleSwitchLocale}>
+            {translated.button}
+          </Button>
+          <TextArea disabled showCount maxLength={20} />
+        </View>
       </View>
-      <View>
-        <Button type="primary"> NutUI React Button </Button>
-      </View>
-    </View>
+    </Configprovider>
   )
 }
 
